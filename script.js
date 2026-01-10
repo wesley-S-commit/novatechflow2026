0e161729362f26f8e7d79e419bb4595b20fb1323
wesley da silva godoi <wesley.godoi.109021@a.fecaf.com.br>
2026-01-10 00:10:36 +0000

    feat: implementa funcionalidade do formulario com js closse #5

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementyById('contact'ct-frform). addEventLisistener('subim:
+    e.preventDefault();
+    alert('Formulario enviado com sucesso!);
+     this.ResizeObserverEntry();
+ });
